451a1669bd4f22464b4faf4e9ee15143575c2bdf
Andrés Esteban Vega <80923229+andresestebanvega@users.noreply.github.com>
2022-11-17 21:36:37 -0300

    1ra parte - tengo errores

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/FIE/Paradigmas 3/Templatecpp
+# Install script for directory: C:/FIE/git/2do_Parcial_PP3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.1.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/MinGW/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/FIE/Paradigmas 3/Templatecpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/FIE/git/2do_Parcial_PP3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "Empresa.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Empresa empresa1("Cosmeticos");
+    empresa1.ingresarVendedor();
+    empresa1.ingresarZona();
+    empresa1.ingresarMontoZona();
+
+    empresa1.mostrarEmpresa();//no estaba puesto aqui
+
+    //Requerimiento A
+    empresa1.mostrarCantidadZona();
+    //Requerimiento B
+    empresa1.mostrarVendedorMayorRecaudacion();
+    //Requerimiento C
+    empresa1.mostrarVendedorMenorRecaudacion();
     return 0;
 }
